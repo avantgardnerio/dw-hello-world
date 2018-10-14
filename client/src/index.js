34cb111cc415d3f924c7552120012f0bab3e4e5e
Brent Gardner <bgardner@squarelabs.net>
2018-10-14 16:48:23 -0600

    Render test job

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+    jobs: [
+        {id: 1, name: 'test'}
+    ]
+};
+const reducer = (state = initialState, action) => {
+    console.log('reducer', state, action);
+    return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root')
+);
