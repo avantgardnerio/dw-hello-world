9818982a4ff52f4c2e4e2ecd8b7082710a9d0220
Brent Gardner <bgardner@squarelabs.net>
2018-10-14 11:25:14 -0600

    Add failing test

---
+++ src/test/java/com/mycompany/app/HelloWorldResourceTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldResourceTest {
+    @Test
+    public void exampleTest() {
+        Assert.assertEquals(true, false);
+    }
+}
