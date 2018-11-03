da57f499f4dcf7fbf53f719b5aa9d8e54dfacb9a
Brent Gardner <bgardner@squarelabs.net>
2018-11-02 19:41:14 -0600

    Able to spawn a job

--- src/test/java/com/mycompany/app/KubernetesTest.kt
+++ src/test/java/com/mycompany/app/KubernetesTest.kt
@@ -1,13 +1,14 @@
 package com.mycompany.app
 
-import io.kubernetes.client.ApiClient;
-import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.BatchV1Api
 import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1Pod;
-import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.models.*
+import io.kubernetes.client.proto.V1
+import io.kubernetes.client.proto.V1Batch
 import io.kubernetes.client.util.Config;
 import org.junit.Test
+
 
 class KubernetesTest {
     @Test
@@ -20,5 +21,31 @@ class KubernetesTest {
         for (item in list.items) {
             println(item.metadata.name)
         }
+
+        val container = V1Container()
+        container.name = "pi"
+        container.image = "perl"
+        container.command = listOf("perl", "-Mbignum=bpi", "-wle", "print bpi(2000)")
+
+        val podSpec = V1PodSpec()
+        podSpec.containers = listOf(container)
+        podSpec.restartPolicy = "Never"
+
+        val template = V1PodTemplateSpec()
+        template.spec = podSpec
+
+        val jobSpec = V1JobSpec()
+        jobSpec.template = template
+        jobSpec.backoffLimit = 4
+
+        val metaData = V1ObjectMeta()
+        metaData.name = "pi"
+
+        val job = V1Job()
+        job.metadata = metaData
+        job.spec = jobSpec
+
+        val result = BatchV1Api().createNamespacedJob("default", job, null)
+        println(result)
     }
 }
